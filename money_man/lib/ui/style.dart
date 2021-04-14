c30fe2e644018921bdc8bb511f0fd75fb3006e83
Truong Kim Lam <lamtk.84@gmail.com>
2021-04-14 10:39:39 +0700

    Transaction detail, wallet selection ui

---
+++ money_man/lib/ui/style.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+// Colors
+const Color primaryColor = Color(0xFFFFD54F);
+const Color secondaryColor = Colors.white;
+const Color textColor = Color(0xFF262626);
+const Color highlight = Color(0xFF48CAE4);
+const Color warning = Color(0xFFDC2F02);
+const Color success = Color(0xFF52B788);
+
+// Text styles
+const TextStyle tsAppBar = TextStyle(color: textColor, fontFamily: 'Montserrat', fontWeight: FontWeight.w600, fontSize: 18.0);
+const TextStyle tsButton = TextStyle(color: Colors.white, fontFamily: 'Montserrat', fontWeight: FontWeight.w600,);
+const TextStyle tsRegular = TextStyle(color: textColor, fontFamily: ' Montserrat', fontWeight: FontWeight.w400, fontSize: 15.0);
+const TextStyle tsMain = TextStyle(fontFamily: 'Montserrat', fontWeight: FontWeight.w600, fontSize: 15.0);
+const TextStyle tsChild = TextStyle(fontFamily: 'Montserrat', fontWeight: FontWeight.w500, fontSize: 13.0);
