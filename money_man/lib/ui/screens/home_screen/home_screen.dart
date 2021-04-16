7ba7dbabbe97343936164cf996e7c432fba534c7
TungTLT <tranlethanhtung554@gmail.com>
2021-04-16 19:40:14 +0700

    prepare for merge

--- money_man/lib/ui/screens/home_screen/home_screen.dart
+++ money_man/lib/ui/screens/home_screen/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:money_man/ui/screens/shared_screens/loading_screen.dart';
-import 'package:money_man/ui/screens/transaction_screen.dart';
+import 'package:money_man/ui/screens/transaction_screens/transaction_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -9,10 +9,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
-  static List<Widget> _screens = [
-    TransactionScreen(),
-    LoadingScreen()
-  ];
+  static List<Widget> _screens = [TransactionScreen(), LoadingScreen()];
 
   void _onItemTap(int index) {
     setState(() {
@@ -27,12 +24,30 @@ class _HomeScreenState extends State<HomeScreen> {
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.shifting,
         backgroundColor: Colors.white,
-        items:  <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.account_balance_wallet_rounded, size: 20.0),label: 'Wallet',backgroundColor: Colors.black),
-          BottomNavigationBarItem(icon: Icon(Icons.analytics_sharp), label: 'Report',backgroundColor: Colors.black),
-          BottomNavigationBarItem(icon: Icon(Icons.add_circle, size: 60.0,), label: 'Add',backgroundColor: Colors.black ),
-          BottomNavigationBarItem(icon: Icon(Icons.article_sharp), label: 'Report',backgroundColor: Colors.black),
-          BottomNavigationBarItem(icon: Icon(Icons.account_circle), label: 'Report',backgroundColor: Colors.black),
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_balance_wallet_rounded, size: 20.0),
+              label: 'Wallet',
+              backgroundColor: Colors.black),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.analytics_sharp),
+              label: 'Report',
+              backgroundColor: Colors.black),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.add_circle,
+                size: 60.0,
+              ),
+              label: 'Add',
+              backgroundColor: Colors.black),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.article_sharp),
+              label: 'Report',
+              backgroundColor: Colors.black),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle),
+              label: 'Report',
+              backgroundColor: Colors.black),
         ],
         currentIndex: _selectedIndex,
         onTap: _onItemTap,
