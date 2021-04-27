c7413d295f7a53996ca85bb3e720bb09e2828e98
Truong Kim Lam <lamtk.84@gmail.com>
2021-04-27 23:08:04 +0700

    Fix planning screen not load

--- money_man/lib/ui/screens/home_screen/home_screen.dart
+++ money_man/lib/ui/screens/home_screen/home_screen.dart
@@ -45,7 +45,7 @@ class _HomeScreenState extends State<HomeScreen> {
             TransactionScreen(currentWallet: wallet),
             ReportScreen(),
             LoadingScreen(),
-            ErrorScreen(),
+            PlanningScreen(),
             AccountScreen(),
           ];
 
