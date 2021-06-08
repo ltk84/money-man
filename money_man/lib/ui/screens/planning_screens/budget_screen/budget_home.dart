8a3753edd317d8e1f05ebcdf11b0e9daece012f3
hacThe <19522252@gm.uit.edu.vn>
2021-06-08 08:48:58 +0700

    fix loi stream builder wrap scafford

--- money_man/lib/ui/screens/planning_screens/budget_screen/budget_home.dart
+++ money_man/lib/ui/screens/planning_screens/budget_screen/budget_home.dart
@@ -38,115 +38,112 @@ class _BudgetScreenState extends State<BudgetScreen>
   @override
   Widget build(BuildContext context) {
     final _firestore = Provider.of<FirebaseFireStoreService>(context);
-    //_wallet = _firestore.currentWallet
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: DefaultTabController(
-        length: 3,
-        child: StreamBuilder<Wallet>(
-            stream: _firestore.currentWallet,
-            builder: (context, snapshot) {
-              Wallet wallet = snapshot.data;
-              print(wallet.id);
-              return Scaffold(
-                appBar: AppBar(
-                  backgroundColor: Color(0xff333333),
-                  leading: IconButton(
-                    icon: Icon(Icons.arrow_back),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                  ),
-                  title: Text(
-                    "Budget",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'Montserrat',
-                    ),
-                  ),
-                  actions: [
-                    GestureDetector(
-                      onTap: () async {
-                        final _auth = Provider.of<FirebaseAuthService>(context,
-                            listen: false);
+        debugShowCheckedModeBanner: false,
+        home: DefaultTabController(
+          length: 3,
+          child: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Color(0xff333333),
+              leading: IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              title: Text(
+                "Budget",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Montserrat',
+                ),
+              ),
+              actions: [
+                GestureDetector(
+                  onTap: () async {
+                    final _auth = Provider.of<FirebaseAuthService>(context,
+                        listen: false);
 
-                        final result = await showCupertinoModalBottomSheet(
-                            isDismissible: true,
-                            backgroundColor: Colors.grey[900],
-                            context: context,
-                            builder: (context) {
-                              return Provider(
-                                  create: (_) {
-                                    return FirebaseFireStoreService(
-                                        uid: _auth.currentUser.uid);
-                                  },
-                                  child: WalletSelectionScreen(
-                                    id: wallet.id,
-                                  ));
-                            });
-                      },
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          SuperIcon(
-                            iconPath: wallet.iconID,
-                            size: 30,
-                          ),
-                          Icon(Icons.arrow_drop_down),
-                        ],
+                    final result = await showCupertinoModalBottomSheet(
+                        isDismissible: true,
+                        backgroundColor: Colors.grey[900],
+                        context: context,
+                        builder: (context) {
+                          return Provider(
+                              create: (_) {
+                                return FirebaseFireStoreService(
+                                    uid: _auth.currentUser.uid);
+                              },
+                              child: WalletSelectionScreen(
+                                id: _wallet.id,
+                              ));
+                        });
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      SuperIcon(
+                        iconPath: _wallet.iconID,
+                        size: 30,
                       ),
-                    )
-                  ],
-                  bottom: TabBar(
-                    isScrollable: true,
-                    controller: _tabController,
-                    indicatorColor: Color(0xffd3db00),
-                    indicatorWeight: 3,
-                    labelStyle: TextStyle(fontWeight: FontWeight.bold),
-                    unselectedLabelColor: Colors.white30,
-                    tabs: [
-                      Tab(
-                        child: Container(
-                            child: Icon(
-                          Icons.add_circle,
-                          color: Color(0xFF2FB49C),
-                          size: 35,
-                        )),
-                      ),
-                      Tab(
-                          child: Container(
-                        width: 120,
-                        child: Text(
-                          "Currently applied",
-                        ),
-                      )),
-                      Tab(
-                          child: Container(
-                        width: 120,
-                        child: Text(
-                          "Applied",
-                        ),
-                      )),
+                      Icon(Icons.arrow_drop_down),
                     ],
                   ),
-                  elevation: 3,
-                ),
-                body: TabBarView(
-                  controller: _tabController,
-                  children: [
-                    AddBudget(
-                      tabController: _tabController,
+                )
+              ],
+              bottom: TabBar(
+                isScrollable: true,
+                controller: _tabController,
+                indicatorColor: Color(0xffd3db00),
+                indicatorWeight: 3,
+                labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                unselectedLabelColor: Colors.white30,
+                tabs: [
+                  Tab(
+                    child: Container(
+                        child: Icon(
+                      Icons.add_circle,
+                      color: Color(0xFF2FB49C),
+                      size: 35,
+                    )),
+                  ),
+                  Tab(
+                      child: Container(
+                    width: 120,
+                    child: Text(
+                      "Currently applied",
                     ),
-                    CurrentlyApplied(
-                      wallet: wallet,
+                  )),
+                  Tab(
+                      child: Container(
+                    width: 120,
+                    child: Text(
+                      "Applied",
                     ),
-                    Applied(wallet: wallet)
-                  ],
-                ),
-              );
-            }),
-      ),
-    );
+                  )),
+                ],
+              ),
+              elevation: 3,
+            ),
+            body: StreamBuilder<Object>(
+                stream: _firestore.currentWallet,
+                builder: (context, snapshot) {
+                  _wallet = snapshot.data;
+                  return TabBarView(
+                    controller: _tabController,
+                    children: [
+                      AddBudget(
+                        tabController: _tabController,
+                      ),
+                      CurrentlyApplied(
+                        wallet: _wallet,
+                      ),
+                      Applied(wallet: _wallet)
+                    ],
+                  );
+                }),
+          ),
+        ));
   }
 }
