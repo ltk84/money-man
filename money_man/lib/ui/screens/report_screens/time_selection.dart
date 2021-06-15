0a32c2c2ad4f4e4776b355c350bc932e8eff6460
Lam <lamtk.84@gmail.com>
2021-06-15 11:29:57 +0700

    edit select time range ui in report screens

--- money_man/lib/ui/screens/report_screens/time_selection.dart
+++ money_man/lib/ui/screens/report_screens/time_selection.dart
@@ -4,6 +4,7 @@ import 'package:flutter/scheduler.dart';
 import 'package:intl/intl.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:money_man/core/models/time_range_info_model.dart';
+import 'package:money_man/ui/style.dart';
 import 'package:page_transition/page_transition.dart';
 
 import 'custom_time_range.dart';
@@ -72,85 +73,89 @@ class TimeRangeSelectionState extends State<TimeRangeSelection> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.grey[900],
+        backgroundColor: boxBackgroundColor,
         appBar: AppBar(
           centerTitle: true,
           elevation: 0,
-          backgroundColor: Colors.grey[900],
+          backgroundColor: boxBackgroundColor,
           shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(20.0),
                   topRight: Radius.circular(20.0))),
           title: Text('Select Time Range',
               style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'Montserrat',
-                  fontWeight: FontWeight.w600,
-                  fontSize: 15.0)),
+                fontFamily: 'Montserrat',
+                fontSize: 17.0,
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
+              )),
           leading: CloseButton(),
         ),
-        body: ListView.separated(
-          itemCount: listInfo.length,
-          separatorBuilder: (BuildContext context, int index) => Divider(),
-          itemBuilder: (BuildContext context, int index) {
-            String beginDate = (listInfo[index].begin == null)
-                ? 'Day/Month/Year'
-                : DateFormat('dd/MM/yyyy').format(listInfo[index].begin);
-            String endDate = (listInfo[index].end == null)
-                ? 'Day/Month/Year'
-                : DateFormat('dd/MM/yyyy').format(listInfo[index].end);
+        body: Container(
+          color: backgroundColor1,
+          child: ListView.separated(
+            itemCount: listInfo.length,
+            separatorBuilder: (BuildContext context, int index) => Divider(),
+            itemBuilder: (BuildContext context, int index) {
+              String beginDate = (listInfo[index].begin == null)
+                  ? 'Day/Month/Year'
+                  : DateFormat('dd/MM/yyyy').format(listInfo[index].begin);
+              String endDate = (listInfo[index].end == null)
+                  ? 'Day/Month/Year'
+                  : DateFormat('dd/MM/yyyy').format(listInfo[index].end);
 
-            return ListTile(
-              onTap: () async {
-                if (listInfo[index].description == 'Custom') {
-                  final result = await showCupertinoModalBottomSheet(
-                      isDismissible: true,
-                      backgroundColor: Colors.grey[900],
-                      context: context,
-                      builder: (context) => CustomTimeRange(
-                          beginDate:
-                              _dateDescription == 'Custom' ? _beginDate : null,
-                          endDate:
-                              _dateDescription == 'Custom' ? _endDate : null));
-                  if (result.runtimeType == listInfo[0].runtimeType &&
-                      result != null) {
+              return ListTile(
+                onTap: () async {
+                  if (listInfo[index].description == 'Custom') {
+                    final result = await showCupertinoModalBottomSheet(
+                        isDismissible: true,
+                        backgroundColor: Colors.grey[900],
+                        context: context,
+                        builder: (context) => CustomTimeRange(
+                            beginDate:
+                                _dateDescription == 'Custom' ? _beginDate : null,
+                            endDate:
+                                _dateDescription == 'Custom' ? _endDate : null));
+                    if (result.runtimeType == listInfo[0].runtimeType &&
+                        result != null) {
+                      setState(() {
+                        listInfo.removeLast();
+                        listInfo.add(result);
+                        _dateDescription = listInfo[index].description;
+                      });
+                      Navigator.of(context).pop(result);
+                    }
+                  } else {
+                    var result = TimeRangeInfo(
+                        description: listInfo[index].description,
+                        begin: listInfo[index].begin,
+                        end: listInfo[index].end);
                     setState(() {
-                      listInfo.removeLast();
-                      listInfo.add(result);
                       _dateDescription = listInfo[index].description;
                     });
                     Navigator.of(context).pop(result);
                   }
-                } else {
-                  var result = TimeRangeInfo(
-                      description: listInfo[index].description,
-                      begin: listInfo[index].begin,
-                      end: listInfo[index].end);
-                  setState(() {
-                    _dateDescription = listInfo[index].description;
-                  });
-                  Navigator.of(context).pop(result);
-                }
-              },
-              title: Text(listInfo[index].description,
-                  style: TextStyle(
-                    fontFamily: 'Montserrat',
-                    fontSize: 15,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.white,
-                  )),
-              subtitle: Text(beginDate + " - " + endDate,
-                  style: TextStyle(
-                    fontFamily: 'Montserrat',
-                    fontSize: 14,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.grey[500],
-                  )),
-              trailing: _dateDescription == listInfo[index].description
-                  ? Icon(Icons.check, color: Colors.blueAccent)
-                  : null,
-            );
-          },
+                },
+                title: Text(listInfo[index].description,
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white,
+                    )),
+                subtitle: Text(beginDate + " - " + endDate,
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.grey[500],
+                    )),
+                trailing: _dateDescription == listInfo[index].description
+                    ? Icon(Icons.check_rounded, color: primaryColor)
+                    : null,
+              );
+            },
+          ),
         ));
   }
 }
