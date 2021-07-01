713ca116c240fb70490dbfa9c5ade12dc06b7731
hacThe <19522252@gm.uit.edu.vn>
2021-07-01 16:45:11 +0700

    fommater

--- money_man/lib/ui/screens/account_screens/about_screen.dart
+++ money_man/lib/ui/screens/account_screens/about_screen.dart
@@ -20,10 +20,6 @@ class _AboutScreenState extends State<AboutScreen> {
   int reachTop = 0;
   int reachAppBar = 0;
 
-  //
-  // Text title = Text('My Account', style: TextStyle(fontSize: 30, color: Colors.white, fontFamily: 'Montserrat', fontWeight: FontWeight.bold));
-  // Text emptyTitle = Text('', style: TextStyle(fontSize: 30, color: Colors.white, fontFamily: 'Montserrat', fontWeight: FontWeight.bold));
-
   // Phần này để check xem mình đã Scroll tới đâu trong ListView
   ScrollController _controller = ScrollController();
   _scrollListener() {
@@ -85,33 +81,8 @@ class _AboutScreenState extends State<AboutScreen> {
               ),
             ),
           ),
-          //),
-          //centerTitle: true,
           backgroundColor: Colors.transparent,
           elevation: 0,
-          // flexibleSpace: ClipRect(
-          //   child: AnimatedOpacity(
-          //     opacity: reachAppBar == 1 ? 1 : 0,
-          //     duration: Duration(milliseconds: 0),
-          //     child: BackdropFilter(
-          //       filter: ImageFilter.blur(
-          //           sigmaX: reachTop == 1 ? 25 : 500,
-          //           sigmaY: 25,
-          //           tileMode: TileMode.values[0]),
-          //       child: AnimatedContainer(
-          //         duration: Duration(
-          //             milliseconds:
-          //                 reachAppBar == 1 ? (reachTop == 1 ? 100 : 0) : 0),
-          //         //child: Container(
-          //         //color: Colors.transparent,
-          //         color: Colors.grey[
-          //                 reachAppBar == 1 ? (reachTop == 1 ? 800 : 850) : 900]
-          //             .withOpacity(0.2),
-          //         //),
-          //       ),
-          //     ),
-          //   ),
-          // ),
         ),
         body: ListView(
           physics:
@@ -223,9 +194,10 @@ class _AboutScreenState extends State<AboutScreen> {
   }
 }
 
+// Card về thông tin của thành viên, avt, tên và description
 class InfoCard extends StatelessWidget {
   final String name;
-  final String iconPath;
+  final String iconPath; // path đến icon chỉ định của thành viên
   final Color avatarColor;
   final String fbURL;
   final String igURL;
@@ -247,6 +219,7 @@ class InfoCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
+        // Chuyển đến trang thông tin chi tiết
         Navigator.of(context).push(MaterialPageRoute(
             builder: (context) => InfoCardDetail(
                   name: name,
@@ -317,9 +290,10 @@ class InfoCard extends StatelessWidget {
   }
 }
 
+// Thông tin chi tiết của thành viên
 class InfoCardDetail extends StatelessWidget {
   final String name;
-  final String iconPath;
+  final String iconPath; // icon được chỉ định của thành viên
   final Color avatarColor;
   final String fbURL;
   final String igURL;
@@ -526,10 +500,6 @@ class InfoCardDetail extends StatelessWidget {
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          // SuperIcon(
-                          //   iconPath: 'assets/images/email_2.svg',
-                          //   size: 18,
-                          // ),
                           Icon(
                             Icons.email_rounded,
                             size: 18,
